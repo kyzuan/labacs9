c6b494747fa35bfb9e3ff4f7d94c0b72dfe3976b
heivus <kyzmykin1@gmail.com>
2022-04-12 01:08:27 +0300

    omm

--- labacs9/Block1Var2.cs
+++ labacs9/Block1Var2.cs
@@ -45,13 +45,62 @@ namespace labacs9
     }
     internal class Block1Var2
     {
-
+        static double DoubleValue(MyFrac f)
+        {
+            double result;
+            double nom = f.nom;
+            double denom = f.denom;
+            return nom / denom;
+        }
+        static MyFrac Plus(MyFrac f1, MyFrac f2)
+        {          
+            long nok = 0;
+            for (int i = 0; i < (f2.denom * f1.denom + 1); i++)
+            {
+                if (i % f1.denom == 0 && i % f2.denom == 0)
+                {
+                    nok = i;
+                    if (i != 0)
+                    {  
+                        break;
+                    }
+                }
+            }    
+            MyFrac res = new MyFrac(f1.nom+f2.nom, nok);
+            return res;
+        }
+        
         public static void DoBlock()
         {
+            Console.WriteLine("Введите значение числителя дроби:");
             long n = long.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение знаменателя дроби:");
             long d = long.Parse(Console.ReadLine());
             MyFrac f = new MyFrac(n, d);
+            Console.WriteLine("Полученная дробь: ");
             Console.WriteLine(f.ToString());
+            Console.WriteLine("Истинное значение дроби: ");
+            Console.WriteLine(DoubleValue(f));
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Введите значение числителя первой дроби:");
+            long n1 = long.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение знаменателя первой дроби:");
+            long d1 = long.Parse(Console.ReadLine());
+            MyFrac f1 = new MyFrac(n1, d1);
+            Console.WriteLine("Полученная дробь: ");
+            Console.WriteLine(f1.ToString());
+            Console.WriteLine("Введите значение числителя первой дроби:");
+            long n2 = long.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение знаменателя первой дроби:");
+            long d2 = long.Parse(Console.ReadLine());
+            MyFrac f2 = new MyFrac(n2, d2);
+            Console.WriteLine("Полученная дробь: ");
+            Console.WriteLine(f2.ToString());
+            Console.WriteLine("Результат сложения двух дробей: ");
+            MyFrac res = Plus(f1, f2);
+            Console.WriteLine(res.ToString());
+
 
 
 

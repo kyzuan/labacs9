3729f1e7d417c783cbf8eaa7fc4508fb1f0fb39d
igorshavlak <85722788+igorshavlak@users.noreply.github.com>
2022-04-12 13:01:39 +0300

    Add files via upload

---
+++ Block1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace lab3
+{
+    internal class Program
+    {
+        struct MyTime
+        {
+            public int hour, minute, second;
+            public MyTime(int h, int m, int s)
+            {
+                hour = h; minute = m; second = s;
+            }
+            public override string ToString()
+            {
+                
+                TimeSpan t = new TimeSpan(hour, minute, second);
+                return t.ToString((@"hh\:mm\:ss"));
+
+            }
+        };
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+            MyTime myTime = new MyTime(18, 4, 3);
+            string a = myTime.ToString();
+            Console.WriteLine(a);
+
+        }
+    }
+}
